2ce9ef7cede67d8133d2dd8741b54db41d11da5b
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-11-05 17:41:35 +0330

    sequence_to_path

--- Catalans/catalans.py
+++ Catalans/catalans.py
@@ -1,3 +1,20 @@
 class Catalan:
     def __init__(self):
         print("Catalan is called!")
+
+
+    def sequence_to_path(self, sequence):
+        X = [0]
+        Y = [0]
+        currX = 0
+        currY = 0
+        for s in sequence:
+            if s == 0:
+                currY += 1
+            else:
+                currY -= 1
+            currX += 1
+            X.append(currX)
+            Y.append(currY)
+
+        return(X, Y)
