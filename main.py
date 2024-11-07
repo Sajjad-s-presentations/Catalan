a2ff5fd577c508d97fe401b1261be5a15c6b4417
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-11-07 22:43:04 +0330

    sequence_to_path test plot

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Plots.plot import Plot
 
 C1 = Catalan()
 P1 = Plot()
-path = C1.sequence_to_path('010010101101')
+path = C1.sequence_to_path('0010011010011011')
 print(path)
 P1.line_only_y_plot(path[1])
 
