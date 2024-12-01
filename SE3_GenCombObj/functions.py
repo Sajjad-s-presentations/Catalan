c352734b17c517827d1c0d404661225ca97175f3
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-01 14:47:45 +0330

    permutation

--- SE3_GenCombObj/functions.py
+++ SE3_GenCombObj/functions.py
@@ -2,3 +2,13 @@ class Permutation:
     def __init__(self, m):
         print("Permutation constructor is called with order {}".format(m))
         self.m = m
+        self.partition = []
+
+    def generate_parrtitions(self, curr_m, B, N):
+        if curr_m == 0:
+            return self.partition
+        else:
+            for i in range(1, min(B, N)):
+                an = i
+                self.partition.append(an)
+                self.generate_parrtitions(curr_m-i, i, N+1)
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from SE1_GenCombObj.LexicographicalOrdering import LexographicalOrdering
+from SE3_GenCombObj.functions import Permutation
+
 from Catalans.catalans import Catalan
 from Plots.plot import Plot
 
-C1 = Catalan()
-P1 = Plot()
-path = C1.sequence_to_path('001101')
-print(path)
-P1.line_only_y_plot(path[1])
+p1 = Permutation(3)
 
 
 
