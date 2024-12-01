efc081ba079c01ff3649bdc350a9c0ecc2a52717
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-01 15:03:53 +0330

    debuging permutation

--- SE3_GenCombObj/functions.py
+++ SE3_GenCombObj/functions.py
@@ -5,10 +5,9 @@ class Permutation:
         self.partition = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     def generate_parrtitions(self, curr_m, B, N):
-        print(curr_m)
         if curr_m == 0:
             return self.partition
         else:
             for i in range(1, min(B, curr_m)):
-                self.partition.append(i)
+                self.partition[N+1] = i
                 self.generate_parrtitions(curr_m-i, i, N+1)
